c4773286cca9b643a1c39cc7c2abcd922faa281e
NaMax66 <max.naid@mail.ru>
2021-09-29 23:13:02 +0500

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const { PORT } = require("./config.js")
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/api" && req.method === "GET") {
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    res.write("Init");
+
+    res.end();
+  }
+  else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+})
+
+server.listen(PORT, () => {
+  console.log(`server started on port: ${PORT}`)
+})
